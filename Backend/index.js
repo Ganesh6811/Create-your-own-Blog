dd1d8335019a3f6a861607628a9736799758ab4b
Ganesh S <133591305+Ganesh6811@users.noreply.github.com>
2025-04-22 22:24:16 +0530

    Update the cors for the frontend deployment02

--- Backend/index.js
+++ Backend/index.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 app.use(cookieParser());
 app.use(cors({
-    origin: `*`, 
+    origin: `https://create-your-own-blog-frontend.onrender.com`, 
     credentials: true,  
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],   
     allowedHeaders: ["Content-Type", "Authorization"],   
