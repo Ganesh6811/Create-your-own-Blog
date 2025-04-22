3159dfe1ec6015441d75fec24ad81114eac9e472
Ganesh <ganeshsudhanagunta68@gmail.com>
2025-04-22 22:19:40 +0530

    Update the cors to get the requsts from the frontend01

--- Backend/index.js
+++ Backend/index.js
@@ -19,14 +19,14 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 app.use(cookieParser());
-// app.use(cors({
-//     origin: "http://localhost:3000", 
-//     credentials: true,  
-//     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],   
-//     allowedHeaders: ["Content-Type", "Authorization"],   
-// }));
+app.use(cors({
+    origin: `*`, 
+    credentials: true,  
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],   
+    allowedHeaders: ["Content-Type", "Authorization"],   
+}));
 
-app.options('*', cors());
+
 
 app.use("/api/auth/", AuthRoute);
 app.use("/api/posts/", PostsRoute);
