1636000d9a004b55114417c8a60437ec37ce47b1
Ganesh S <133591305+Ganesh6811@users.noreply.github.com>
2025-04-23 09:20:33 +0530

    Error Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import Login from "./Authentication/LoginPage.jsx";
 import Profile from "./Pages/profile.jsx";
 import CreateBlog from "./components/CreateBlog.jsx";
 import BlogDetails from "./components/BlogDetails.jsx";
+import baseUrl from "./config.js";
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(null);
@@ -14,7 +15,7 @@ function App() {
   useEffect(() => {
     const checkUser = async () => {
       try {
-        const res = await axios.get("http://localhost:5001/api/auth/check", {
+        const res = await axios.get(`${baseUrl}api/auth/check`, {
           withCredentials: true,
         }); 
 
